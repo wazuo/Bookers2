fd9fb87c34ab3d0e17a4da64b2ec95a26e8ccf23
EC2 Default User <95359918+wazuo@users.noreply.github.com>
2021-12-18 08:27:36 +0000

    [Update]4-10 book.show indexの作成

--- config/routes.rb
+++ config/routes.rb
@@ -1,4 +1,6 @@
 Rails.application.routes.draw do
+  get "about" => "homes#about"
   devise_for :users
   root to: 'homes#top'
+  resources :books, only: [:index, :edit, :create, :show, :update, :destroy]
 end
