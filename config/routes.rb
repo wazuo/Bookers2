d7cfdd8717517d531a6c801814b5cdddc9af7fc7
EC2 Default User <95359918+wazuo@users.noreply.github.com>
2021-12-18 12:58:43 +0000

    [Update]header作成中

--- config/routes.rb
+++ config/routes.rb
@@ -1,7 +1,7 @@
 Rails.application.routes.draw do
-  get 'users/show'
   get "about" => "homes#about"
   devise_for :users
   root to: 'homes#top'
   resources :books, only: [:index, :edit, :create, :show, :update, :destroy]
+  resources :users, only: [:show, :edit, :update]
 end
