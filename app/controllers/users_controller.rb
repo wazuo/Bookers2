d3913160b4dfe9cfaa3f09edfeda9c9cd6891fb1
EC2 Default User <95359918+wazuo@users.noreply.github.com>
2021-12-19 12:50:42 +0000

    [update]viewfile作成

--- app/controllers/users_controller.rb
+++ app/controllers/users_controller.rb
@@ -1,5 +1,9 @@
 class UsersController < ApplicationController
   def show
-    @user =User.find(params[])
+    @user =User.find(params[:id])
+    @books = @user.books.all
+  end
+  def edit
+    @user = User.find(params[:id])
   end
 end
