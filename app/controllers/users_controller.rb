f3064d041593ace967d12a1ae2c8ec232f420f26
EC2 Default User <95359918+wazuo@users.noreply.github.com>
2021-12-31 14:56:54 +0000

    [update]

--- app/controllers/users_controller.rb
+++ app/controllers/users_controller.rb
@@ -2,7 +2,6 @@ class UsersController < ApplicationController
 
   def index
     @book = Book.new
-    @books = Book.all
     @user = current_user
     @users = User.all
   end
@@ -22,12 +21,6 @@ class UsersController < ApplicationController
     end
   end
 
-  def create
-    @book = Book.new(book_params)
-    @book.user_id = current_user.id
-    @book.save
-    redirect_to book_path(@book)
-  end
 
   def update
     @user =User.find(params[:id])
